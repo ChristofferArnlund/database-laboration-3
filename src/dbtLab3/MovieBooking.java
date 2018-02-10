dd50b3be303cb55934d140d420fc7e687beb1b1c
Christoffer Arnlund <christoffer.arnlund@gmail.com>
2018-02-10 17:41:06 +0100

    added database

--- src/dbtLab3/MovieBooking.java
+++ src/dbtLab3/MovieBooking.java
@@ -1,7 +1,7 @@
 package dbtLab3;
 
 /**
- * MovieBooking is the main class for the movie ticket booking 
+ * MovieBooking is the main class for the movie ticket booking
  * application. It creates a database object and the GUI to
  * interface to the database.
  */
